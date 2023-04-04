929eff98562b4a038269741a9753aecc294d3771
seasaw-game <99113512+seasaw-game@users.noreply.github.com>
2023-04-04 23:24:21 +0900

    Update oppy_app.py

--- oppy_app.py
+++ oppy_app.py
@@ -12,29 +12,27 @@ class BAIN:
     def __init__(self, speed):
         self.speed = speed
 
-    def oppy_update(self, t):
+    def oppy_update(self, t, line):
         y = np.linspace(-3, 3, 100)
         x1 = 1.5*np.exp((0.12*np.sin(t)-0.5)*(y+0.16*np.sin(t))**2)/(1+np.exp(-20*(5*y+np.sin(t))))
         x2 = (1.5+0.8*(y+0.2*np.sin(t))**3)*(1+np.exp(20*(5*y+np.sin(t))))**(-1)/(1+np.exp(-1*(100*(y+1)+16*np.sin(t))))
         x3 = 0.2*(np.exp(-1*(y+1)**2)+1)/(1+np.exp(100*(y+1)+16*np.sin(t)))
         x4 = 0.1/np.exp(2*(10*y+1.2*(2+np.sin(t))*np.sin(t))**4)
         x = x1 + x2 + x3 + x4
-        self.ax.cla()
-        self.ax.plot(x,y, color="black")
+        line.set_data(x, y)
+#         self.ax.cla()
+#         self.ax.plot(x,y, color="black")
+        return line,
 
     def animate(self):
         fig = plt.figure(figsize=(3,8))
-        self.ax = fig.add_subplot(111)
-        # y = np.linspace(-3, 3, 100)
-        # frames = []
-        # num_frames = 100
-        # for i in range(num_frames):
-        #     frame = plt.plot(self.x_set(i*0.1, y), y, color="black")
-        #     frames.append(frame)
+#         self.ax = fig.add_subplot(111)
+        l, = plt.plot([], [], "b-")
 
         # 描画
 #         ani = ArtistAnimation(fig, frames, interval=110-self.speed*10)
-        ani = FuncAnimation(fig, self.oppy_update, frames=range(1000), interval=110-self.speed*10)
+#         ani = FuncAnimation(fig, self.oppy_update, frames=range(100), interval=110-self.speed*10
+        ani = FuncAnimation(fig, self.oppy_update, frames=range(100),fargs=(l) interval=100)
         components.html(ani.to_jshtml(),height=800)
 
 #         display.HTML(ani.to_jshtml())
