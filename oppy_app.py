9e8ab582b43f61e111d26ced0759fe488feb855b
seasaw-game <99113512+seasaw-game@users.noreply.github.com>
2023-04-04 22:32:44 +0900

    Update oppy_app.py

--- oppy_app.py
+++ oppy_app.py
@@ -1,9 +1,10 @@
-%matplotlib widget
+
 import streamlit as st
 import streamlit.components.v1 as components
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib.animation import ArtistAnimation
+# from matplotlib.animation import ArtistAnimation
+from matplotlib.animation import FuncAnimation
 from IPython import display 
 
 
@@ -21,6 +22,16 @@ class BAIN:
         x = x1 + x2 + x3 + x4
         x = x.astype(np.float64)
         return x
+    
+    def oppy_update(t):
+        y = np.linspace(-3, 3, 100)
+        x1 = 1.5*np.exp((0.12*np.sin(t)-0.5)*(y+0.16*np.sin(t))**2)/(1+np.exp(-20*(5*y+np.sin(t))))
+        x2 = (1.5+0.8*(y+0.2*np.sin(t))**3)*(1+np.exp(20*(5*y+np.sin(t))))**(-1)/(1+np.exp(-1*(100*(y+1)+16*np.sin(t))))
+        x3 = 0.2*(np.exp(-1*(y+1)**2)+1)/(1+np.exp(100*(y+1)+16*np.sin(t)))
+        x4 = 0.1/np.exp(2*(10*y+1.2*(2+np.sin(t))*np.sin(t))**4)
+
+        x = x1 + x2 + x3 + x4
+        ax.plot(x,y, color="black")
 
     def animate(self):
         fig = plt.figure(figsize=(3,8))
@@ -32,7 +43,8 @@ class BAIN:
             frames.append(frame)
 
         # 描画
-        ani = ArtistAnimation(fig, frames, interval=110-self.speed*10)
+#         ani = ArtistAnimation(fig, frames, interval=110-self.speed*10)
+        ani = FuncAnimation(fig, oppy_update, frames=range(100), interval=110-self.speed*10)
         components.html(ani.to_jshtml(),height=800)
         
 #         display.HTML(ani.to_jshtml())
