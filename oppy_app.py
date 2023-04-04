08d6fb1ee8e42600270d3a9231f6f72f640ff38e
seasaw-game <99113512+seasaw-game@users.noreply.github.com>
2023-04-04 23:37:45 +0900

    Update oppy_app.py

--- oppy_app.py
+++ oppy_app.py
@@ -8,14 +8,14 @@ from matplotlib.animation import ArtistAnimation
 from IPython import display
 
 def x_set(t, y):
-    y = np.linspace(-3, 3, 100)
+    
     x1 = 1.5*np.exp((0.12*np.sin(t)-0.5)*(y+0.16*np.sin(t))**2)/(1+np.exp(-20*(5*y+np.sin(t))))
     x2 = (1.5+0.8*(y+0.2*np.sin(t))**3)*(1+np.exp(20*(5*y+np.sin(t))))**(-1)/(1+np.exp(-1*(100*(y+1)+16*np.sin(t))))
     x3 = 0.2*(np.exp(-1*(y+1)**2)+1)/(1+np.exp(100*(y+1)+16*np.sin(t)))
     x4 = 0.1/np.exp(2*(10*y+1.2*(2+np.sin(t))*np.sin(t))**4)
     
     x = x1 + x2 + x3 + x4
-    x = x.astype(np.float64)
+#     x = x.astype(np.float64)
     return x
 
 # Streamlit app
